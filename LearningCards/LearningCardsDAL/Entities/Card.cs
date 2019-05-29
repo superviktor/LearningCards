ebf63b67b336d7477bd2777dfa92c6a8333005eb
Viktor Prykhidko <victor.pryhidko@gmail.com>
2019-05-29 16:31:08 +0300

    DAL and basic entities and repos

---
+++ LearningCards/LearningCardsDAL/Entities/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LearningCardsDAL.Entities
+{
+    public class Card: Entity
+    {
+        public int Number { get; set; }
+
+        public  Deck Deck { get; set; }
+
+        public ICollection<ContentSection> ContentSections { get; set; }
+    }
+}
---
+++ LearningCards/LearningCardsDAL/Entities/ContentSection.cs
@@ -0,0 +1,8 @@
+﻿namespace LearningCardsDAL.Entities
+{
+    public class ContentSection:Entity
+    {
+        public Card Card { get; set; }
+        public string Content { get; set; }
+    }
+}
